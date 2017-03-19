6c0bd5aaac0bcf3d2c855a051354c2dccce69974
ihgazni2 <286264978@qq.com>
2017-03-19 15:47:17 +0800

    add CrtableLib

--- xdict/__init__.py
+++ xdict/__init__.py
@@ -1,3 +1,3 @@
 
-__all__ = ['utils','ltdict','jprint','hdict_object','hdict_cmdline','hdict_xml','tuple_list','console_color','TestLib']
+__all__ = ['utils','ltdict','jprint','hdict_object','hdict_cmdline','hdict_xml','tuple_list','console_color','TestLib','CrtableLib']
 
