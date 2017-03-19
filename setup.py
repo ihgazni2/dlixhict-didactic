a1eab17b25a286e81295702c590e8b9b9a597cb5
ihgazni2 <286264978@qq.com>
2017-03-19 11:10:50 +0800

    add setup.py and uninstall.sh

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+      name="xdict",
+      version="0.00",
+      description="tools for transform between html,dict and cmdline",
+      author="dapeli",
+      url="https://github.com/ihgazni2/dlixhict-didactic",
+      author_email='terryinzaghi@163.com', 
+      license="MIT",
+      packages= find_packages(),
+      py_modules=['xdict'], 
+      )
+
+
+# python3 setup.py bdist --formats=tar
+# python3 setup.py sdist
+
