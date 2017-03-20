c77152eb51593b434fea1a9aaddc429a0e96581c
ihgazni2 <286264978@qq.com>
2017-03-20 21:31:40 +0800

    remove old build

--- updateversion.sh
+++ updateversion.sh
@@ -1,3 +1,13 @@
+
+python3 setup.py install --record install.txt
+cat install.txt | xargs rm -rf
+rm -r dist
+rm -r build
+rm -r xdict.egg-info
 git rm -r dist
 git rm -r build
 git rm -r xdict.egg-info
+git add .
+git commit -m "remove old build"
+python3 setup.py install --record install.txt
+
