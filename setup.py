276b9ecb4ebe672698ac2eba47a923f4a664d029
ihgazni2 <286264978@qq.com>
2017-03-31 12:55:29 +0800

    move utils.md to directory ReadMe

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
       name="xdict",
-      version = "0.38",
+      version = "0.39",
       description="tools for transform between html,dict and cmdline",
       author="dapeli",
       url="https://github.com/ihgazni2/dlixhict-didactic",
