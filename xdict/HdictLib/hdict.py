783889b519cd71b76b8a1677bcc0de9c414a0415
ihgazni2 <286264978@qq.com>
2017-05-10 12:09:10 +0800

    remove old build

---
+++ xdict/HdictLib/hdict.py
@@ -0,0 +1,10 @@
+from xdict import utils
+from xdict import ltdict
+from xdict import jprint
+from xdict import cmdline
+from xdict import hdict_object
+from xdict import hdict_xml
+
+class hdict():
+    def __init__(**kwargs):
+        pass    
