ac82993b1d76525208c241446c38aa42d76e70f8
ihgazni2 <286264978@qq.com>
2018-01-25 16:45:13 +0800

    remove old build

--- xdict/jsfunc.py
+++ xdict/jsfunc.py
@@ -21,3 +21,51 @@ def logical_and(x,y):
     else:
         return(x)
 
+
+def newDate_num(**kwargs):
+    n = time.time()
+    s = str(n)
+    arr = s.split(".")
+    sec_bitsnum = arr[0].__len__()
+    ms_bitsnum = sec_bitsnum + 3
+    us_bitsnum = ms_bitsnum + 3
+    s = s.replace(".","")
+    if('bitsnum' in kwargs):
+        bitsnum = kwargs['bitsnum']
+    else:
+        bitsnum = ms_bitsnum
+        if('unit' in kwargs):
+            unit = kwargs['unit']
+        else:
+            unit = "ms"
+        if(unit == "s"):
+            bitsnum = sec_bitsnum     
+        elif(unit == "ms"):
+            bitsnum = ms_bitsnum
+        elif(unit == "us"):
+            bitsnum = us_bitsnum
+        else:
+            return(int(s))
+    return(int(s[:bitsnum]))
+
+
+
+def toString(n,radix,**kwargs):
+    if('char_set' in kwargs):
+        char_set = kwargs['char_set']
+    else:
+        char_set = "0123456789abcdefghijklmnopqrstuvwxyz"
+    r = n % radix
+    q = n // radix
+    rslt = char_set[r]
+    n = q
+    while(q>=radix):
+        r = n % radix
+        q = n // radix
+        rslt = rslt + char_set[r]
+        n = q
+    rslt = rslt + str(char_set[q])
+    rslt = rslt[::-1]
+    return(rslt)
+
+
