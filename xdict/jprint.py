a0520baf1042ad2493130a7331411bd855c4a736
ihgazni2 <286264978@qq.com>
2018-01-30 22:49:28 +0800

    remove old build

--- xdict/jprint.py
+++ xdict/jprint.py
@@ -474,7 +474,7 @@ def convert_token_in_quote(j_str,**kwargs):
         tmp_regex = fsm.creat_regex_not_from_arr(tmp_final_arr)
         machine.add(sn,tmp_regex,do_replace,sn)
         #####
-    #print(machine.fsm_dict)
+    print(machine.fsm_dict)
     curr_state = "INIT"
     rslt = ''
     for i in range(0,j_str.__len__()):
