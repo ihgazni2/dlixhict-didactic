11b98d3cd4219c3cbfb2533c1310ebeb8f414ff0
dli <terryinzaghi@163.com>
2019-10-04 15:16:51 +0000

    remove old build

--- xdict/bin/creat_apis_from_verb.py
+++ xdict/bin/creat_apis_from_verb.py
@@ -1,22 +1,10 @@
-#import sys
 from xdict.ide import *
 from efuntool.efuntool import dflt_sysargv
-
-#def dflt_sysargv(dflt,which):
-#    try:
-#        rslt = sys.argv[which]
-#        print(which,rslt)
-#    except:
-#        rslt = dflt
-#    else:
-#        pass
-#    return(rslt)
 
 
 def main():
     pre = dflt_sysargv("",1)
     verb = dflt_sysargv("return",2)
     suf = dflt_sysargv("",3)
-    print(sys.argv,pre,verb,suf)
     creat_apis_from_verb(pre,verb,suf)
 
