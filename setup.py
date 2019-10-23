593612bfe1e0b3f685d64e9600091f58a0de801e
dli <terryinzaghi@163.com>
2019-10-23 08:33:59 +0000

    remove old build

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
       name="xdict",
-      version = "1.0.8",
+      version = "1.0.9",
       description="tools for transform between html,dict and cmdline",
       author="dapeli",
       url="https://github.com/ihgazni2/dlixhict-didactic",
